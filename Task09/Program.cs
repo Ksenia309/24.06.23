3c62a0bdcc36821b65a364b6cf554c5ee3d2dff7
Ksenia <ks.keyno@gmail.com>
2023-07-13 16:57:56 +0300

    Added Task18

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -7,3 +7,19 @@
 
 int number = new Random().Next(10, 100); // 99 + 1
 Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine($"Наибольшая цифра числа: {firstDigit}");
+}
+else if (firstDigit < secondDigit)
+{
+    Console.WriteLine($"Наибольшая цифра числа: {secondDigit}");
+}
+else
+{
+    Console.WriteLine($"Цифры одинаковые");
+}
