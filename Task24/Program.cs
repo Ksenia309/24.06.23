3677c27629906a96afbd9733787c1064c290dc17
Ksenia <ks.keyno@gmail.com>
2023-07-15 16:17:58 +0300

    Added Task24

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -6,5 +6,27 @@
 // 4 -> 10
 // 8 -> 36
 
-Console.WriteLine("Введите целое положительное число");
-int number = Convert.ToInt32(Console.ReadLine())
+Console.WriteLine("Введите целое положительное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 0)
+{
+    Console.WriteLine("Некорректный ввод!");
+    return;
+}
+
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumbers}");
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            sum = sum + i; //sum += i;
+        }
+    }
+    return sum;
+}
