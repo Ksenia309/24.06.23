e9adfab1963ea65a177888065ed9dacfe778a4b9
Ksenia <ks.keyno@gmail.com>
2023-08-04 21:29:27 +0300

    Added Task65

--- Task65/Program.cs
+++ Task65/Program.cs
@@ -4,9 +4,9 @@
 // M = 1; N = 5 -> "1, 2, 3, 4, 5"
 // M = 4; N = 8 -> "4, 5, 6, 7, 8"
 
-Console.WriteLine("Введите первое натуральное число");
+Console.WriteLine("Введите первое натуральное число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе натуральное число");
+Console.WriteLine("Введите второе натуральное число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
 NaturalNumbers(number1, number2);
